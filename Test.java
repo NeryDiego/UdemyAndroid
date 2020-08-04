e4ee2fd77fe7ada7059dc46232713bbbf496a9a2
Diego <nerydiego85@gmail.com>
2020-08-04 13:55:28 -0500

    Test.java
    Example for Context

---
+++ Test.java
@@ -0,0 +1,11 @@
+package com.example.activitylifecircle;
+
+/*
+Ejemplo de Context y Activity
+*/
+public class Test {
+
+    public static void ActivityTest(Context context){
+        Toast.makeText(context, "Hola Mundo desde Java Class", Toast.LENGTH_LONG).show();
+    }
+}
