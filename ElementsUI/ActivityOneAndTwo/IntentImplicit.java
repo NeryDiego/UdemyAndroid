b2f0d7a9d6c611d4368f306e2ff0861419459526
NeryDiego <47128620+NeryDiego@users.noreply.github.com>
2020-08-06 18:04:05 -0500

    Update IntentImplicit.java

--- ElementsUI/ActivityOneAndTwo/IntentImplicit.java
+++ ElementsUI/ActivityOneAndTwo/IntentImplicit.java
@@ -2,13 +2,22 @@ package com.example.seccion01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+package com.example.seccion01;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class IntentImplicit extends AppCompatActivity {
 
@@ -17,6 +26,7 @@ public class IntentImplicit extends AppCompatActivity {
     private ImageButton imgBtnPhone;
     private ImageButton imgBtnName;
     private ImageButton imgBtnCamera;
+    private final static int PHONE_CALL_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +44,50 @@ public class IntentImplicit extends AppCompatActivity {
             public void onClick(View view) {
                 String phoneNumber = editTextPhone.getText().toString();
                 if(editTextPhone.getText() != null){
-                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+phoneNumber));
+                    //comprobar versión actual de android
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+                    } else {
+                        olderVersions(phoneNumber);
+                    }
+                }
+            }
+
+            private void olderVersions(String phoneNumber){
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+phoneNumber));
+                if(CheckPermissions(Manifest.permission.CALL_PHONE)){
                     startActivity(intent);
+                } else {
+                    Toast.makeText(IntentImplicit.this, "You decline the access", Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PHONE_CALL_CODE:
+                String permission = permissions[0];
+                int result = grantResults[0];
+
+                if(permission.equals(Manifest.permission.CALL_PHONE)){
+                    //Comprobar si ha sido aceptado o no la petición
+                    if(result == PackageManager.PERMISSION_GRANTED){
+                        String phoneNumber = editTextPhone.getText().toString();
+                        Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+phoneNumber));
+                        startActivity(intentCall);
+                    }
+                }
+                break;
+        }
+    }
+
     /*
-    Revisamos los permisos de la aplicación
-     */
+        Revisamos los permisos de la aplicación
+         */
     private boolean CheckPermissions(String permission){
         int result = this.checkCallingOrSelfPermission(permission);
         return result == PackageManager.PERMISSION_GRANTED;
-    }    
+    }
 }
