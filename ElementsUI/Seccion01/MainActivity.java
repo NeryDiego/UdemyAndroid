8ff676e5fa109a3db14d5cccc1f4198c450f8b77
NeryDiego <47128620+NeryDiego@users.noreply.github.com>
2020-08-06 16:39:47 -0500

    onClickListener
    Con esta función podemos remover el metodo "miBoton" y aún así leer cuando se clickea el boton

--- ElementsUI/Seccion01/MainActivity.java
+++ ElementsUI/Seccion01/MainActivity.java
@@ -7,11 +7,20 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        btn = (Button) findViewById(R.id.buttonMain); //Este método regresa una "view"
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Hola desde SetOn", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     /*
